453cef2fb3c53eb6b2d0cd47ea7c5ad5b665ebf4
sachin97581 <sachin97581@gmail.com>
2025-06-03 14:38:17 +0530

    Initial commit

---
+++ UberReviewService/src/main/java/com/example/UberReviewService/Models/Review.java
@@ -0,0 +1,13 @@
+package com.example.UberReviewService.Models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    Long id;
+
+}
